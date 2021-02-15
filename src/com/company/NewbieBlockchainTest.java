96a386ad862246027659646cbde360405766f1f2
rushatgabhane <rushatgabhane@gmail.com>
2021-02-15 16:37:33 +0300

    fix random index for test

--- src/com/company/NewbieBlockchainTest.java
+++ src/com/company/NewbieBlockchainTest.java
@@ -2,13 +2,17 @@ package com.company;
 
 import jdk.jfr.Description;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class NewbieBlockchainTest {
 
     @Test
+    @DisplayName("Should test if isBlockchainValid is working properly")
     void testDetectionOfInvalidBlockchain() {
         NewbieBlockchain.blockchain.add(new Node("0", "First Node"));
         NewbieBlockchain.blockchain.add(new Node(NewbieBlockchain.blockchain.get(NewbieBlockchain.blockchain.size()-1).hash, "Second Node"));
@@ -16,8 +20,10 @@ class NewbieBlockchainTest {
 
         assertTrue(NewbieBlockchain.isBlockchainValid());
 
+        Random random = new Random();
+        int index = random.nextInt(NewbieBlockchain.blockchain.size());
         // change a random node
-        NewbieBlockchain.blockchain.set(0, new Node("0", "Hacked"));
+        NewbieBlockchain.blockchain.set(index, new Node("0", "Hacked"));
 
         assertFalse(NewbieBlockchain.isBlockchainValid());
     }
